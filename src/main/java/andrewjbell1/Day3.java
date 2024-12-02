d4e7f7a4cf2f728959a0683d2457ae32e1b41f07
Andrew Bell <andrew.bell@starlingbank.com>
2024-12-02 17:22:07 +0000

    Day 3 template

---
+++ src/main/java/andrewjbell1/Day3.java
@@ -0,0 +1,23 @@
+package andrewjbell1;
+
+public class Day3 extends Day {
+
+    public Day3(int day) {
+        super(day);
+    }
+
+    public static void main(String[] args) {
+        var day = new Day3(3);
+        day.runPart1();
+        day.runPart2();
+    }
+
+    public String part1(String input) {
+      return "";
+    }
+
+    public String part2(String input) {
+      return "";
+    }
+    
+}
