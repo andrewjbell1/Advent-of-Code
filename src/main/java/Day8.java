492b4ee8c3e52608acaced9efc57f2477f45f97a
Andrew Bell <andrewbell@Andrews-MacBook-Air.local>
2024-12-14 18:26:55 +0000

    Day 8 part 2

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -24,7 +24,6 @@ public class Day8 extends Day{
                     var antenna = grid[y][x];
                     scanForMatchingAntenna(antPosition, antenna, grid, antinodePositions);
                 }
-
             }
         }
 
@@ -41,14 +40,36 @@ public class Day8 extends Day{
                     Position positionDiff = antPosition.subtract(currentPosition);
                     Position antiNodePosition = currentPosition.subtract(positionDiff);
                     if (currentPosition.x() != antPosition.x() && currentPosition.y() != antPosition.y()) {
-                        if (antiNodePosition.y() < grid.length && antiNodePosition.x() < grid[0].length && antiNodePosition.y() >= 0 && antiNodePosition.x() >= 0) {
-                            System.out.println("Adding antinode at: " + antiNodePosition + " for antenna: " + antenna + " at position " + antPosition + " based on matching ant at: " + currentPosition);
+                        if (antiNodePosition.inGrid(grid)) {
                             antinodePositions.add(antiNodePosition);
                         }
-                    } else {
-                        System.out.println("NOT adding antinode at: " + antiNodePosition + " for antenna: " + antenna + " at position " + antPosition + " based on matching ant at: " + currentPosition);
                     }
+                }
+            }
+        }
+    }
 
+    private void scanForMatchingAntennaPart2(Position antPosition, char antenna, char[][] grid, HashSet<Position> antinodePositions) {
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                Position currentPosition = Position.of(x, y);
+                char possibleAntenna = grid[currentPosition.y()][currentPosition.x()];
+                if (possibleAntenna == antenna) {
+                    Position positionDiff = antPosition.subtract(currentPosition);
+
+                    while (true) {
+                        Position antiNodePosition = currentPosition.subtract(positionDiff);
+                        if (antiNodePosition.inGrid(grid)) {
+                            antinodePositions.add(antiNodePosition);
+                        }
+                        else{
+                            break;
+                        }
+                        if (positionDiff.x()==0 && positionDiff.y()==0){ // Only add antenna once
+                            break;
+                        }
+                        currentPosition = antiNodePosition;
+                    }
                 }
             }
         }
@@ -56,6 +77,20 @@ public class Day8 extends Day{
 
     @Override
     public long part2(String input) {
-        return 0;
+        var grid = asMatrix(input);
+        var antinodePositions = new HashSet<Position>();
+
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] !='.'){
+                    var antPosition = Position.of(x, y);
+                    var antenna = grid[y][x];
+                    scanForMatchingAntennaPart2(antPosition, antenna, grid, antinodePositions);
+                }
+
+            }
+        }
+        antinodePositions.forEach(System.out::println);
+        return antinodePositions.size();
     }
 }
