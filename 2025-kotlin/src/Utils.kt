e7acf6b6e2d7053be8b3eca11a0ff140a651fac3
Andrew Bell <andrew.bell@starlingbank.com>
2025-12-02 14:57:09 +0000

    Day 2

--- 2025-kotlin/src/Utils.kt
+++ 2025-kotlin/src/Utils.kt
@@ -1,6 +1,7 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
-fun readInput(name: String) = Path("2025-kotlin/src/$name.txt").readText().trim().lines()
+fun readInputOfLines(name: String) = Path("2025-kotlin/src/$name.txt").readText().trim().lines()
+fun readInputOfCommaSeperated(name: String) = Path("2025-kotlin/src/$name.txt").readText().trim().split(",")
 
 fun Any?.println() = println(this)
---
+++ 2025-kotlin/src/day02/Day02.kt
@@ -0,0 +1,60 @@
+package day01
+
+import println
+import readInputOfCommaSeperated
+import readInputOfLines
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        var total = 0L
+        input.forEach {
+            val rangeString = it.split("-")
+            LongRange(rangeString[0].toLong(), rangeString[1].toLong())
+                .filter { it.toString().length.mod(2) == 0 }
+                .forEach {
+                    val left = it.toString().subSequence(0, it.toString().length.div(2)).toString().toLong()
+                    val right =
+                        it.toString().subSequence(it.toString().length.div(2), it.toString().length).toString().toLong()
+                    if (left == right) total += it
+                }
+        }
+
+        return total
+    }
+
+    fun Long.hasDuplicates(): Boolean  {
+        val rangeItemString = this.toString()
+        1.rangeTo(rangeItemString.length).map { possibleSequenceLength ->
+            val possibleSequence = rangeItemString.substring(0, possibleSequenceLength)
+            val maxNumberOfSequencesThisLength = rangeItemString.length.div(possibleSequenceLength)
+
+            if (maxNumberOfSequencesThisLength <= 1) return false
+            val shouldMatchOriginalString = possibleSequence.repeat(maxNumberOfSequencesThisLength)
+
+            if (shouldMatchOriginalString == rangeItemString) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.flatMap {
+            val (start, end) = it.split("-").map { it.toLong() }
+            LongRange(start, end)
+        }.filter { it.hasDuplicates()}.sum()
+    }
+
+    val example = readInputOfCommaSeperated("day02/example")
+    val input = readInputOfCommaSeperated("day02/input")
+
+    check(part1(example) == 1227775554L)
+    "Part 1:".println()
+    part1(input).println()
+
+    check(part2(example) == 4174379265L)
+    println(part2(example))
+
+    "Part 2:".println()
+    part2(input).println()
+}
