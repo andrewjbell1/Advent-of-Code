78f744f18e92cbbf6b12a4b35b734f7e57fb5bdf
Andrew Bell <andrew.bell@starlingbank.com>
2025-12-04 18:18:34 +0000

    Working

--- 2025-kotlin/src/Utils.kt
+++ 2025-kotlin/src/Utils.kt
@@ -1,5 +1,7 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.Duration
+import kotlin.time.measureTime
 
 fun readInputOfLines(name: String) = Path("2025-kotlin/src/$name.txt").readText().trim().lines()
 fun readInputOfCommaSeperated(name: String) = Path("2025-kotlin/src/$name.txt").readText().trim().split(",")
@@ -8,6 +10,11 @@ fun Any?.println() = println(this)
 
 fun <E> Iterable<E>.indexesOf(predicate: (E) -> Boolean) = mapIndexedNotNull{ index, elem -> index.takeIf{ predicate(elem) } }
 
+fun benchmark(times: Int = 1000, body: () -> Unit): Duration {
+    val duration = measureTime { repeat(times) { body() } }.div(times)
+    println("Average Duration: $duration")
+    return duration
+}
 
 fun check(actual: Any, expected: Any) {
     if (actual != expected) {
--- 2025-kotlin/src/day3/Day03.kt
+++ 2025-kotlin/src/day3/Day03.kt
@@ -1,5 +1,6 @@
 package day3
 
+import benchmark
 import check
 import indexesOf
 import println
@@ -23,38 +24,42 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return input.map { line ->
-            val digitList = line.trim().split("")
+        return input.sumOf { line ->
+            var remainingDigits = line.trim().split("")
                 .filter { it.isNotEmpty() }
                 .map { it.toInt() }.toList()
 
-            val max = digitList.subList(0, digitList.size - 12).max()
-
-            val joltageIndex = setOf(digitList.indexOf(max)).toSortedSet()
-
-            // find the next highest 12 numbers
-            digitList.indexesOf { it == max }
-                .flatMap { iMax ->
-                    val subList = digitList.subList(iMax + 1, digitList.size)
-
-                    //return index
+            var digitsRequiredCount = 11
+            var maxFromDigits: List<Int>
+            var joltage = ""
+            while (digitsRequiredCount >= 0) {
+                maxFromDigits = remainingDigits.subList(0, remainingDigits.size - digitsRequiredCount)
+                var maxIndex = 0
+                for (i in maxFromDigits.indices) {
+                    if (maxFromDigits[i] > maxFromDigits[maxIndex]) {
+                        maxIndex = i
+                    }
                 }
-                .map { "$max$it".toLong() }
-                .max()
+                joltage += maxFromDigits[maxIndex].toString()
+                remainingDigits = remainingDigits.subList(maxIndex + 1, remainingDigits.size)
+                digitsRequiredCount--
+            }
+            joltage.toLong()
+        }
 
-        }.sum()
     }
 
     val example = readInputOfLines("day3/example")
     val input = readInputOfLines("day3/input")
 
-
     check(part1(example), 357)
     "Part 1:".println()
     part1(input).println()
+    benchmark(10) { part1(input) } //5.17ms
 
-    check(part2(example),3121910778619)
+    check(part2(example), 3121910778619)
     println(part2(example))
     "Part 2:".println()
     part2(input).println()
+    benchmark(10) { part2(input) } //3.43ms
 }
